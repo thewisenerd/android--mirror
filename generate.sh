e04d667e10cde6ca7f813e4a96b386ea8fd70abd
thewisenerd <thewisenerd@protonmail.com>
2017-09-29 19:25:56 +0530

    generate: make reproducible

--- generate.sh
+++ generate.sh
@@ -2,17 +2,25 @@
 
 REPO=git@github.com:LineageOS/android.git
 
-# rm -rf android.git
-# git clone --mirror $REPO
+cleanup() {
+	rm -rf android.git
+	for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.0; do
+		rm -rf $branch
+	done
+	rm -rf aio
+	rm -rf aio-sort-uniq
+}
 
-rm -rf aio
+cleanup
 
+git clone --mirror $REPO
 for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.0; do
-	# rm -rf $branch
-	# git clone android.git -b $branch $branch
+	git clone android.git -b $branch $branch
 	./parse.py $branch/default.xml | sort >> aio
 done
 
 cat aio | sort | uniq > aio-sort-uniq
 
-# git clone --single-branch $REPO -b cm-14.1
+./merge.py aio-sort-uniq > default.xml
+
+cleanup
