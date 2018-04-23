07b6b7f0e59ac87e042729bccba841f5608d28a0
thewisenerd <thewisenerd@protonmail.com>
2018-04-23 19:09:45 +0530

    lineage-15.0 -> lineage-15.1

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,7 +6,7 @@ REPO=git@github.com:LineageOS/android.git
 
 cleanup() {
 	rm -rf android.git
-	for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.0; do
+	for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.1; do
 		rm -rf $branch
 	done
 	rm -rf aio
@@ -16,7 +16,7 @@ cleanup() {
 cleanup
 
 git clone --mirror $REPO
-for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.0; do
+for branch in cm-11.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.1; do
 	git clone android.git -b $branch $branch
 	scripts/parse.py $branch/default.xml | sort >> aio
 done
